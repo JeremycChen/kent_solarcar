c6a3cae98bd9dcc37ab50a2631eddbd2e143a056
David Zhou <51421093+zzpDavid2@users.noreply.github.com>
2024-07-13 00:42:26 +0800

    Update gps_test.py

--- tests/gps_test.py
+++ tests/gps_test.py
@@ -6,6 +6,6 @@ with Serial('COM7', 115200, timeout=3) as stream:
     raw_data, parsed_data = nmr.read()
     if parsed_data is not None:
         print(parsed_data)
-        print(parsed_data.lat)
-        print(parsed_data.long)
-        print(parsed_data.spd)
+        print("lat: " + parsed_data.lat)
+        print("long: " + parsed_data.long)
+        print("spd: " +parsed_data.spd)
