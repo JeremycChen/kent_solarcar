00cd327c889f48ca76493332129f08d8f52d3ec0
David Zhou <51421093+zzpDavid2@users.noreply.github.com>
2024-07-13 00:41:51 +0800

    Update gps_test.py

--- tests/gps_test.py
+++ tests/gps_test.py
@@ -1,7 +1,11 @@
 from serial import Serial
 from pynmeagps import NMEAReader
 with Serial('COM7', 115200, timeout=3) as stream:
-  nmr = NMEAReader(stream)
-  raw_data, parsed_data = nmr.read()
-  if parsed_data is not None:
-    print(parsed_data)
+  while True:
+    nmr = NMEAReader(stream)
+    raw_data, parsed_data = nmr.read()
+    if parsed_data is not None:
+        print(parsed_data)
+        print(parsed_data.lat)
+        print(parsed_data.long)
+        print(parsed_data.spd)
