cffaf510207e6fccc4f395fa2ee08bf9230a08c5
David Zhou <51421093+zzpDavid2@users.noreply.github.com>
2024-07-13 00:45:53 +0800

    Update gps_test.py

--- tests/gps_test.py
+++ tests/gps_test.py
@@ -5,7 +5,10 @@ with Serial('COM7', 115200, timeout=3) as stream:
     nmr = NMEAReader(stream)
     raw_data, parsed_data = nmr.read()
     if parsed_data is not None:
-        print(parsed_data)
-        print("lat: " + parsed_data.lat)
-        print("long: " + parsed_data.long)
-        print("spd: " +parsed_data.spd)
+        try:
+            print(parsed_data)
+            print("lat: " + str(parsed_data.lat))
+            print("long: " + str(parsed_data.long))
+            print("spd: " + str(parsed_data.spd))
+        except Exception as e:
+            print(f"No gps data: {e}")
